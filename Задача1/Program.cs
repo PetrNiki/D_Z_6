f7207c1077926c7a21dbece4d1a15e47c291cd80
PetrNiki <nikitenkopv@yandex.ru>
2022-08-08 07:57:15 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 0, 7, 8, -2, -2 -> 2;
+Console.WriteLine("Введите числа через пробел");
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Nambers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine(Nambers(array));
